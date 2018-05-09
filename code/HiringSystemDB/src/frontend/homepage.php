dc62e3de4f37093ad5d9ffa30523f7f4cd7a1a09
Deniz Alkışlar <alkislardeniz@gmail.com>
2018-05-09 16:52:15 +0300

    Initial commit
    Initial commit

---
+++ code/HiringSystemDB/src/frontend/homepage.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="homepage.php">Portakal</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="homepage.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Page 2</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
+</nav>
+  
+
+<div class="container">
+  <h1>Welcome</h1>
+  <div class="btn-group btn-group-justified">
+    <div class="btn-group">
+      <button type="button" class="btn btn-primary">Apple</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn btn-primary">Samsung</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn btn-primary">Sony</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn btn-primary">Sony</button>
+    </div>
+    <div class="btn-group">
+      <button type="button" class="btn btn-primary">Sony</button>
+    </div>        
+  </div>
+   <form  action="/action_page.php">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+</div>
+
+</body>
+</html>
