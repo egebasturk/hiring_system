f814a524645c3b34472023bb2cca857a4d94c44d
Deniz Alkışlar <alkislardeniz@gmail.com>
2018-05-10 08:43:20 +0300

    Logon for pro users

---
+++ code/HiringSystemDB/src/frontend/logon_pro.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="homepage.php">Portakal</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="homepage.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Page 1-1</a></li>
+          <li><a href="#">Page 1-2</a></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Page 2</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="login.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    </ul>
+  </div>
+</nav>
+  
+<div class="container">
+  <h1>Welcome</h1>
+  <p>List of actions for professional users</p>
+  <div class="btn-group btn-group-justified">
+    <a href="#" class="btn btn-warning">Manage Account</a>
+    <a href="#" class="btn btn-warning">View Proposals</a>
+    <a href="#" class="btn btn-warning">View Request</a>
+    <a href="#" class="btn btn-warning">View Services</a>
+    <a href="#" class="btn btn-warning">Service Registration</a>
+  </div>
+</div>
+
+</body>
+</html>
