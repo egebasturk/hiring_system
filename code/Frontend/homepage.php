18241fee0246edfb260560f6e7c9b4d597ceb91e
Alp Ege Baştürk <alp.ege.basturk@gmail.com>
2018-05-12 20:07:46 +0300

    Signup link added to button.

--- code/Frontend/homepage.php
+++ code/Frontend/homepage.php
@@ -40,7 +40,7 @@
       <li><a href="#">Page 2</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
-      <li><a href=""><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+      <li><a href="signup_reg.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
       <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
   </div>
