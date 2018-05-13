ce83165c55cc8b7bd661e09b271d1778411e8a3b
alkislardeniz <alkislardeniz@gmail.com>
2018-05-13 16:29:12 +0300

    Navbar added

--- code/Frontend/view_service_requests_reg.php
+++ code/Frontend/view_service_requests_reg.php
@@ -65,6 +65,28 @@
 </style>
 <body>
 
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="homepage.php">Portakal</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="homepage.php">Home</a></li>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Page 1-1</a></li>
+                    <li><a href="#">Page 1-2</a></li>
+                    <li><a href="#">Page 1-3</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Page 2</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="login.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+        </ul>
+    </div>
+</nav>
+
 <div class="container">
     <h1>Service Requests</h1>
     <table class="table table-bordered">
